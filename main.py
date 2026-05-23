e41b54e00b82ad2a5f410c9e2d2b9e92c5518369
vk22006 <kishorev22006@gmail.com>
2026-05-23 23:40:22 +0530

    chore: Create main file

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+GlyphAtlas - Unicode to Codepoint Converter
+Main entry point
+"""
+
+from app import GlyphAtlasApp
+
+if __name__ == "__main__":
+    app = GlyphAtlasApp()
+    app.mainloop()
